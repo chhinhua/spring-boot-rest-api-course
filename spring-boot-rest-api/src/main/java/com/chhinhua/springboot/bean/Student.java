fdfeed07cc26e7cf5af3f9c16e62b93a051bd7b0
chhinhua <chhinhua@gmail.com>
2023-03-17 16:03:35 +0700

    Section 4: Spring Boot REST API Development Basics - Important Annotations

--- spring-boot-rest-api/src/main/java/com/chhinhua/springboot/bean/Student.java
+++ spring-boot-rest-api/src/main/java/com/chhinhua/springboot/bean/Student.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Chhin_Hua - 17/03
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Component
 public class Student {
 
     private int id;
